7aab75fd6d18f55b64a6cd7689c3fee8a143adde
roryeiffe <roryeiffe8@gmail.com>
2021-11-22 13:02:14 -0500

    Wrote code for conditional renders and state

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import User from "./components/User";
+import Employee from './components/Employee';
+import Customer from "./components/Customer";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <h1>App</h1> */}
+      {/* <User/>
+      <Employee/> */}
+      <Customer/>
     </div>
   );
 }
